584dd39e92e6c0dca22d89450b8ca878fd97cce6
shalini30062002 <141170920+shalini30062002@users.noreply.github.com>
2023-08-18 13:20:46 +0530

    Create preprocessing data.py

---
+++ Skill Assessment/QUESTION 2/preprocessing data.py
@@ -0,0 +1,36 @@
+import re
+import string
+
+def clean_text(text):
+    # Convert to lowercase
+    text = text.lower()
+
+    # Remove HTML tags (if applicable)
+    text = re.sub(r'<.*?>', '', text)
+
+    # Remove special characters and numbers
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+
+    # Tokenization
+    tokens = text.split()
+
+    # Remove punctuation
+    tokens = [word.strip(string.punctuation) for word in tokens]
+
+    # Remove stopwords (if you have a custom list)
+    custom_stopwords = ['the', 'and', 'in', 'to', 'of', 'is', 'it', 'this', 'for', 'with']
+    tokens = [word for word in tokens if word not in custom_stopwords]
+
+    # Join tokens back to text
+    cleaned_text = ' '.join(tokens)
+
+    return cleaned_text
+
+# Example text
+raw_text = "This is an example sentence with <html> tags and punctuation! 123"
+
+# Clean the text
+cleaned_text = clean_text(raw_text)
+
+print("Original Text:", raw_text)
+print("Cleaned Text:", cleaned_text)
